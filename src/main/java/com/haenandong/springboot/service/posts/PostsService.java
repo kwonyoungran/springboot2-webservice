b888d23699f1d93fef2e00c1511476203d06635a
youngran <youngran@terafunding.com>
2020-07-13 17:12:09 +0900

    20200713_Ch04_05

--- src/main/java/com/haenandong/springboot/service/posts/PostsService.java
+++ src/main/java/com/haenandong/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.haenandong.springboot.service.posts;
 
 import com.haenandong.springboot.domain.posts.Posts;
 import com.haenandong.springboot.domain.posts.PostsRepository;
+import com.haenandong.springboot.web.dto.PostsListResponseDto;
 import com.haenandong.springboot.web.dto.PostsResponseDto;
 import com.haenandong.springboot.web.dto.PostsSaveRequestDto;
 import com.haenandong.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -30,5 +34,18 @@ public class PostsService {
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)     //readOnly=true -> 트랜잭션 범위는 유지하되 조회기능만 남겨두어 조회속도 개선, 등록/수정/삭제 기능 없는 서비스 메소드에서 사용
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new) //-> .map(posts -> new PostsListResponseDto(posts))
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" +id));
+        postsRepository.delete(posts);
     }
 }
